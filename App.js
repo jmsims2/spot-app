8a1588f3943822ad5435210f0efd70676ec6af78
jmsims2 <jmsims2@gmail.com>
2021-09-12 17:19:33 -0600

    clean up app.js

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
-import React, {useState} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  TouchableOpacity,
-  Image,
-  Button,
-  TouchableWithoutFeedback,
-} from 'react-native';
-import {ViroText, ViroARSceneNavigator} from '@viro-community/react-viro';
-import BottomSheet from 'reanimated-bottom-sheet';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import {SpotSceneAR} from './src/screens/SpotView';
-import {SpotControls} from './src/screens/SpotControls';
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { ViroARSceneNavigator } from '@viro-community/react-viro';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SpotSceneAR } from './src/screens/SpotView';
+import { SpotControls } from './src/screens/SpotControls';
 
 export default () => {
   return (
@@ -32,12 +23,5 @@ export default () => {
 };
 
 var styles = StyleSheet.create({
-  f1: {flex: 1},
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
+  f1: { flex: 1 },
 });
